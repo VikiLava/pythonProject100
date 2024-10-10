ced3fbb66159d8a0150287e05dab2394b8d1795e
Lavanya <lavanya.ramesh.in@gmail.om>
2024-10-10 13:53:52 +0530

    Coomita

---
+++ Data types and strings/Lab001 H.w.py
@@ -0,0 +1,24 @@
+#Create a program using user input  and print the table""
+
+num=3.42152335525
+formatted_num = f"{num:.4f}"
+print(formatted_num)
+
+# Tables:
+
+table= int(input("Enter the num"))
+print (f"{table}*1 = {table*1}")
+print (f"{table}*2 = {table*2}")
+print (f"{table}*3 = {table*3}")
+print (f"{table}*4 = {table*4}")
+print (f"{table}*5 = {table*5}")
+print (f"{table}*6 = {table*6}")
+print (f"{table}*7 = {table*7}")
+print (f"{table}*8 = {table*8}")
+print (f"{table}*9 = {table*9}")
+print (f"{table}*10 = {table*10}")
+
+
+#table= 8
+#print(f"{table}*1 = {table*1}")
+#print(f"{table}*2 = {table*2}")
---
+++ Data types and strings/Lab002 H.W.py
@@ -0,0 +1,17 @@
+# Create a program take a input from user num1 and num2
+
+num1 =  (max(100.14522785,85))
+print(num1)
+formatted_num=(f"{num1:.3f}")
+print(formatted_num)
+
+num2 = (pow(2,4))
+print(num2)
+
+number1= int (input("Enter the value 1"))
+number2=  int (input("Enter the value 1"))
+print("sum = ",number1+number2)
+print("sub = ",number1-number2)
+print("mul = ",number1*number2)
+print("Div = ",number1/number2)
+
